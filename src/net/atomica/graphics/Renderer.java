40a835305a69a522ddd93765c68506e7adca1b01
sixreals <ry_walt@posteo.net>
2022-10-03 12:37:27 +0800

    game renders black bg

--- src/net/atomica/graphics/Renderer.java
+++ src/net/atomica/graphics/Renderer.java
@@ -3,19 +3,23 @@ package net.atomica.graphics;
 import net.atomica.game.Game;
 
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.VolatileImage;
 
 
 public class Renderer {
     private static Frame frame;
     private static Canvas canvas;
 
-    private static int canvasHeight;
-    private static int canvasWidth;
+    private static int canvasHeight = 400;
+    private static int canvasWidth = 400;
 
     public static void init() {
         frame = new Frame();
@@ -26,7 +30,6 @@ public class Renderer {
         frame.add(canvas);
         frame.pack(); //sets frame to fit canvas
         frame.setResizable(false); //stops resizing of canvas
-        frame.setVisible(true);
 
         //listen for window events
         frame.addWindowListener(new WindowAdapter() {
@@ -34,5 +37,43 @@ public class Renderer {
                 Game.quit();
             }
         });
+
+        frame.setVisible(true);
+
+        startRendering();
+    }
+
+    private static void startRendering() {
+        Thread thread = new Thread() {
+            public void run() {
+                GraphicsConfiguration gc = canvas.getGraphicsConfiguration();
+                VolatileImage vImage = gc.createCompatibleVolatileImage(canvasWidth, canvasHeight);
+
+                while(true) {
+                    //if volatile image is deleted, recreate it
+                    if(vImage.validate(gc) == VolatileImage.IMAGE_INCOMPATIBLE) {
+                        vImage = gc.createCompatibleVolatileImage(canvasWidth, canvasHeight);
+                    }
+
+                    Graphics g = vImage.getGraphics();
+
+                    //create bg
+                    g.setColor(Color.BLACK);
+                    g.fillRect(0, 0, canvasWidth, canvasHeight);
+
+                    //render stuff
+
+                    g.dispose();
+
+                    g = canvas.getGraphics();
+                    g.drawImage(vImage, 0, 0, canvasWidth, canvasHeight, null);
+
+                    g.dispose();
+                }
+            }
+        };
+
+        thread.setName("Rendering Thread");
+        thread.start();
     }
 }
